408947d858f584f5203184c30b475e0fcf4cba37
Juliano Martins <julianogm@ciandt.com>
2022-06-06 08:53:24 -0300

    estrutura inicial

---
+++ app/config-mouse.sh
@@ -0,0 +1,13 @@
+## Lista dos mauses suportados
+# https://github.com/libratbag/libratbag/tree/master/data/devices
+
+# Instalar o Driver
+sudo apt install ratbagd
+
+# Instalar a Interface Grafica Piper
+sudo apt-add-repository ppa:libratbag-piper/piper-libratbag-git
+sudo apt install piper 
+
+
+## Fonte
+# https://diolinux.com.br/sistemas-operacionais/configure-o-seu-mouse-logitech-no-linux.html
---
+++ install.sh
@@ -0,0 +1,5 @@
+# Executar comandos a seguir para atualizar os pacotes
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#Criar um Loop para rodar os arquivos dentro da pasta APP
